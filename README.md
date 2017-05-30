82170b8d59db98869cc73d97f23c6b88bb746b4b
Roberto <roberto@TitosNotebook.titos.home>
2017-05-29 23:28:01 -0400

    Agrega demo graficando los datos leidos

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MindWave-Python
 Acceso al headset MindWave (RF) desde Python
+
+![](images/Figure1.png)
+
+![](images/Figure2.png)
---
+++ TestGraphics.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from rcr.mindwave.MindWave import MindWave
+from rcr.utils import Utils
+
+
+def main():
+    plt.ion()
+    attentionESense = []
+    meditationESense = []
+    delta = []
+    theta = []
+
+    npts = 20
+    mw = MindWave( "/dev/ttyUSB0", 1000, 0x00, 0x00 )
+    if( mw.connect() ):
+        while( True ):
+            try:
+                mwd = mw.getMindWaveData()
+                print mwd.poorSignalQuality, mwd.attentionESense, mwd.meditationESense, mwd.delta, mwd.theta
+                attentionESense.append( mwd.attentionESense );
+                meditationESense.append( mwd.meditationESense );
+                delta.append( mwd.delta )
+                theta.append( mwd.theta )
+
+                plt.figure( 1 )
+                plt.clf()
+                plt.title( "MindWave Headset" )
+                plt.grid( True )
+                plt.ylim( 0, 100 )
+                plt.plot( attentionESense, "ro-", label="attentionESense" )
+                plt.plot( meditationESense, "b^-", label="meditationESense" )
+                plt.legend( loc="upper left" )
+                plt.text( 0, 10, "Signal Quality: %d" % mwd.poorSignalQuality )
+
+                plt.figure( 2 )
+                plt.clf()
+                plt.title( "MindWave Headset" )
+                plt.grid( True )
+                #plt.ylim( 0, 16777215 )
+                plt.plot( delta, "ro-", label="delta" )
+                plt.plot( theta, "g^-", label="theta" )
+                plt.legend( loc="upper left" )
+
+                plt.draw_all()
+                plt.pause( 0.0001 )
+
+                if( npts > 0 ):
+                    npts = npts - 1
+                else:
+                    attentionESense.pop( 0 )
+                    meditationESense.pop( 0 )
+                    delta.pop( 0 )
+                    theta.pop( 0 )
+            except Exception as e:
+                print e
+                break
+        mw.disconnect()
+
+        """
+        while True: # While loop that loops forever
+            dataArray = [ 0, 0 ]
+            dataArray[0] = random.randint( 800, 900 ) / 10.0
+            dataArray[1] = random.randint( 934500, 935250 ) / 10.0
+            temp = float( dataArray[0])            #Convert first element to floating number and put in temp
+            P =    float( dataArray[1])            #Convert second element to floating number and put in P
+            tempF.append(temp)                     #Build our tempF array by appending temp readings
+            pressure.append(P)                     #Building our pressure array by appending P readings
+            drawnow(makeFig)                       #Call drawnow to update our live graph
+            plt.pause(.000001)                     #Pause Briefly. Important to keep drawnow from crashing
+            cnt=cnt+1
+            if(cnt>50):                            #If you have 50 or more points, delete the first one from the array
+                tempF.pop(0)                       #This allows us to just see the last 50 data points
+                pressure.pop(0)
+        """
+
+if( __name__ == "__main__" ):
+    main()
